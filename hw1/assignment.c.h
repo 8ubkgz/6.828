8f5fa9a76ec4ca0cdbd136d05a5acae8fff66d3c
kooos91 <kos.sklyar91@gmail.com>
2015-11-03 16:28:35 +0000

    add ignore

--- hw1/assignment.c.h
+++ hw1/assignment.c.h
@@ -5,11 +5,12 @@ search_exe(const char *exe) {
 
 	char *path = getenv("PATH");
 	char *tok = path;
+	char *savetok;
 	DIR *d = NULL;
 	struct dirent *dir = NULL;
 	char* ret_path;
 
-      	while(NULL != (tok = strtok(tok, ":"))) {
+      	while(NULL != (tok = strtok_r(tok, ":", &savetok))) {
 		if (NULL != (d = opendir(tok))) {
 			while(NULL != (dir = readdir(d))) {
 				if (0 == strcmp(dir->d_name, exe)) {
