cfc0721a76dc673fb70847cb55bf9bde042b81c3
kooos91 <kos.sklyar91@gmail.com>
2015-11-24 21:55:31 +0000

    lab 2 kernel address space

--- kern/pmap.c
+++ kern/pmap.c
@@ -176,6 +176,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, UPAGES, npages * sizeof(struct PageInfo), PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -188,6 +189,7 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, KSTACKTOP-KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -197,6 +199,7 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, KERNBASE, (uint32_t)(-1) - KERNBASE, 0, PTE_W|PTE_U);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
@@ -386,6 +389,15 @@ static void
 boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm)
 {
 	// Fill this function in
+	size = ROUNDUP(size, PGSIZE);
+	for (int i = 0; i < size; i+= PGSIZE) {
+		pte_t *pte = pgdir_walk(pgdir, (void*)(va+i), true);
+		*pte = (pa+i) | perm | PTE_P;
+		pgdir[PDX(va+i)] |= perm | PTE_P;
+		if ((va+i) >= 4294963200LL) { // ??
+				break;
+		}
+	}
 }
 
 //
@@ -672,6 +684,7 @@ check_kern_pgdir(void)
 
 	// check PDE permissions
 	for (i = 0; i < NPDENTRIES; i++) {
+		//warn("i %u : ", i);
 		switch (i) {
 		case PDX(UVPT):
 		case PDX(KSTACKTOP-1):
